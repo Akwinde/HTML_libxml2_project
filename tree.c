fb13196bc0ecb19a2c590d2ae8d62c3c8b31e206
Juan Carlos Orlando <jcorlandomc@yahoo.com>
2021-02-04 11:39:59 -0800

    02/04/2021 11:39 am

--- tree.c
+++ tree.c
@@ -8,12 +8,24 @@
 
 const char* get_token(int * loop_counter, int * token_counter, char * token)
 {
-	if(*loop_counter == 1 && *token_counter == 3)  { return token; }			// LATITUDE
-	else if(*loop_counter == 1 && *token_counter == 4)  { return token; }		// LONGITUDE
-	else if(*loop_counter == 2 && *token_counter == 3)  { return token; }		// 32.7336
-	else if(*loop_counter == 2 && *token_counter == 4)  { return token; }		// -117.1831
-	else if(*loop_counter == 1 && *token_counter == 15) { return token; }		// HLY-DEWP-NORMAL
-	else if(*loop_counter == 1 && *token_counter == 23) { return token; }		// HLY-TEMP-NORMAL
+	if(*loop_counter == 2 && *token_counter == 3)								// 32.7336
+	{
+		return token;
+	}
+	else if(*loop_counter == 2 && *token_counter == 4)							// -117.1831
+	{
+		return token;
+	}
+	else if(*loop_counter == 1 && *token_counter == 15)							// HLY-DEWP-NORMAL
+	{
+		return token;
+	}
+
+	else if(*loop_counter == 1 && *token_counter == 23)							// HLY-TEMP-NORMAL
+	{
+		return token;
+	}
+
 	else return "";																// return Nothing
 
 }
@@ -48,97 +60,100 @@ int main(int argc, char **argv)
     xmlNodePtr title = xmlNewChild(head, NULL, BAD_CAST "title", BAD_CAST "Assignment 1");
 
 
+
+
 	//  	My code starts here
 
-	FILE *the_file = fopen("data.csv", "r");
-    if (the_file == NULL)
-    {
-        perror("Unable to open the file.");
-        exit(1);
-    }
+	xmlNodePtr table = xmlNewChild(body, NULL, BAD_CAST "table", NULL);
+	xmlNewProp(table, BAD_CAST "align", BAD_CAST "left");
+	xmlNewProp(table, BAD_CAST "border", BAD_CAST "1");
+	xmlNewProp(table, BAD_CAST "cellspacing", BAD_CAST "0");
+	xmlNewProp(table, BAD_CAST "cellpadding", BAD_CAST "0");
+	xmlNewProp(table, BAD_CAST "width", BAD_CAST "100%");
+	xmlNodePtr caption = xmlNewChild(table, NULL, BAD_CAST "caption", NULL);
+	xmlNodePtr strong = xmlNewChild(caption, NULL, BAD_CAST "strong", BAD_CAST "Temperature and Dew Point for SAN DIEGO INTERNATIONAL AIRPORT, CA US");
 
+
+	xmlNodePtr thead = xmlNewChild(table, NULL, BAD_CAST "thead", NULL);
+	xmlNodePtr tr1 = xmlNewChild(thead, NULL, BAD_CAST "tr", NULL);
+	xmlNodePtr th1 = xmlNewChild(tr1, NULL, BAD_CAST "th", NULL);
+	xmlNewProp(th1, BAD_CAST "rowspan", BAD_CAST "2");
+	xmlNodePtr img = xmlNewChild(th1, NULL, BAD_CAST "img", NULL);
+	xmlNewProp(img, BAD_CAST "src", BAD_CAST "noaa.png");
+	xmlNewProp(img, BAD_CAST "alt", BAD_CAST "NOAA Logo");
+	xmlNewProp(img, BAD_CAST "border", BAD_CAST "0");
+	xmlNewProp(img, BAD_CAST "align", BAD_CAST "left");
+	xmlNewProp(img, BAD_CAST "width", BAD_CAST "100");
+	xmlNewProp(img, BAD_CAST "height", BAD_CAST "");
+	xmlNodePtr th2 = xmlNewChild(tr1, NULL, BAD_CAST "th", NULL);
+	xmlNodePtr strong2 = xmlNewChild(th2, NULL, BAD_CAST "strong", BAD_CAST "LATITUDE");
+	xmlNodePtr th3 = xmlNewChild(tr1, NULL, BAD_CAST "th", NULL);
+	xmlNodePtr strong3 = xmlNewChild(th3, NULL, BAD_CAST "strong", BAD_CAST "LONGITUDE");
+
+
+	xmlNodePtr tr2 = xmlNewChild(thead, NULL, BAD_CAST "tr", NULL);
+
+	xmlNodePtr th4 = xmlNewChild(tr2, NULL, BAD_CAST "th", NULL);
+	xmlNodePtr strong4 = xmlNewChild(th4, NULL, BAD_CAST "strong", BAD_CAST "Fill This in");		// Fill This in
+	xmlNodePtr th5 = xmlNewChild(tr2, NULL, BAD_CAST "th", NULL);
+	xmlNodePtr strong5 = xmlNewChild(th5, NULL, BAD_CAST "strong", BAD_CAST "Fill This in");		// Fill This in
+
+
+	xmlNodePtr tbody = xmlNewChild(table, NULL, BAD_CAST "tbody", NULL);
+	xmlNodePtr tr3 = xmlNewChild(tbody, NULL, BAD_CAST "tr", NULL);
+	xmlNodePtr td1 = xmlNewChild(tr3, NULL, BAD_CAST "td", NULL);
+	xmlNodePtr strong6 = xmlNewChild(td1, NULL, BAD_CAST "strong", BAD_CAST "DATE");
+	xmlNodePtr td2 = xmlNewChild(tr3, NULL, BAD_CAST "td", NULL);
+	xmlNodePtr strong7 = xmlNewChild(td2, NULL, BAD_CAST "strong", BAD_CAST "Fill This in");		// Fill this in
+	xmlNodePtr td3 = xmlNewChild(tr3, NULL, BAD_CAST "td", NULL);
+	xmlNodePtr strong8 = xmlNewChild(td3, NULL, BAD_CAST "strong", BAD_CAST "Fill This in");		// Fill this in
+
+
+
+	// This code reads from file input
+
+	FILE *the_file = fopen("data.csv", "r");
+	if (the_file == NULL)
+	{
+		perror("Unable to open the file.");
+		exit(1);
+	}
 
 	int loop_counter = 1;
 	int token_counter = 1;
 	char line[1024];
-    while(fgets(line, sizeof(line), the_file))
-    {
-        char * token;
+	while(fgets(line, sizeof(line), the_file))
+	{
+		char * token;
 		char * string;
-        token = strtok(line, ",");
+		token = strtok(line, ",");
 
 		printf("%i: ", loop_counter);
-        while(token != NULL)
-        {
-			// Print function here
+		while(token != NULL)
+		{
+			// My functions are here
+
 			string = get_token(&loop_counter, &token_counter, token);
 			printf("%s", string);
-			// Print function here
 
-            if(token_counter == 1 && loop_counter > 1)
+			// My functions are here
+
+			if(token_counter == 1 && loop_counter > 1)
 			{
 				token = strtok(NULL, "\"");
 			}
 			else token = strtok(NULL, ",");
 			token_counter++;
-        }
-        printf("\n");
+		}
+		printf("\n");
 		token_counter = 1;
 		loop_counter++;
-    }
+	}
+	// This code reads from file input
 
 
-			xmlNodePtr table = xmlNewChild(body, NULL, BAD_CAST "table", NULL);
-			xmlNewProp(table, BAD_CAST "align", BAD_CAST "left");
-			xmlNewProp(table, BAD_CAST "border", BAD_CAST "1");
-			xmlNewProp(table, BAD_CAST "cellspacing", BAD_CAST "0");
-			xmlNewProp(table, BAD_CAST "cellpadding", BAD_CAST "0");
-			xmlNewProp(table, BAD_CAST "width", BAD_CAST "100%");
-			xmlNodePtr caption = xmlNewChild(table, NULL, BAD_CAST "caption", NULL);
-			xmlNodePtr strong = xmlNewChild(caption, NULL, BAD_CAST "strong", BAD_CAST "Temperature and Dew Point for SAN DIEGO INTERNATIONAL AIRPORT, CA US");
-
-
-			xmlNodePtr thead = xmlNewChild(table, NULL, BAD_CAST "thead", NULL);
-			xmlNodePtr tr1 = xmlNewChild(thead, NULL, BAD_CAST "tr", NULL);
-			xmlNodePtr th1 = xmlNewChild(tr1, NULL, BAD_CAST "th", NULL);
-			xmlNewProp(th1, BAD_CAST "rowspan", BAD_CAST "2");
-			xmlNodePtr img = xmlNewChild(th1, NULL, BAD_CAST "img", NULL);
-			xmlNewProp(img, BAD_CAST "src", BAD_CAST "noaa.png");
-			xmlNewProp(img, BAD_CAST "alt", BAD_CAST "NOAA Logo");
-			xmlNewProp(img, BAD_CAST "border", BAD_CAST "0");
-			xmlNewProp(img, BAD_CAST "align", BAD_CAST "left");
-			xmlNewProp(img, BAD_CAST "width", BAD_CAST "100");
-			xmlNewProp(img, BAD_CAST "height", BAD_CAST "");
-			xmlNodePtr th2 = xmlNewChild(tr1, NULL, BAD_CAST "th", NULL);
-			xmlNodePtr strong2 = xmlNewChild(th2, NULL, BAD_CAST "strong", BAD_CAST "LATITUDE");
-			xmlNodePtr th3 = xmlNewChild(tr1, NULL, BAD_CAST "th", NULL);
-			xmlNodePtr strong3 = xmlNewChild(th3, NULL, BAD_CAST "strong", BAD_CAST "LONGITUDE");
-
-
-			xmlNodePtr tr2 = xmlNewChild(thead, NULL, BAD_CAST "tr", NULL);
-
-			xmlNodePtr th4 = xmlNewChild(tr2, NULL, BAD_CAST "th", NULL);
-			xmlNodePtr strong4 = xmlNewChild(th4, NULL, BAD_CAST "strong", BAD_CAST "32.7336");
-			xmlNodePtr th5 = xmlNewChild(tr2, NULL, BAD_CAST "th", NULL);
-			xmlNodePtr strong5 = xmlNewChild(th5, NULL, BAD_CAST "strong", BAD_CAST "117.1831");
-
-
-			xmlNodePtr tbody = xmlNewChild(table, NULL, BAD_CAST "tbody", NULL);
-			xmlNodePtr tr3 = xmlNewChild(tbody, NULL, BAD_CAST "tr", NULL);
-			xmlNodePtr td1 = xmlNewChild(tr3, NULL, BAD_CAST "td", NULL);
-			xmlNodePtr strong6 = xmlNewChild(td1, NULL, BAD_CAST "strong", BAD_CAST "DATE");
-			xmlNodePtr td2 = xmlNewChild(tr3, NULL, BAD_CAST "td", NULL);
-			xmlNodePtr strong7 = xmlNewChild(td2, NULL, BAD_CAST "strong", BAD_CAST "HLY-DEWP-NORMAL");
-			xmlNodePtr td3 = xmlNewChild(tr3, NULL, BAD_CAST "td", NULL);
-			xmlNodePtr strong8 = xmlNewChild(td3, NULL, BAD_CAST "strong", BAD_CAST "HLY-TEMP-NORMAL");
-
-
-			xmlNodePtr tr4 = xmlNewChild(tbody, NULL, BAD_CAST "tr", NULL);
-
-
-
-
-
+	// While loop to fill the remaining tables
+	// xmlNodePtr tr4 = xmlNewChild(tbody, NULL, BAD_CAST "tr", NULL);
 
 
 	//		My code ends here
